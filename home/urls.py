ce7324d42142a32da2f2f415b5cce57763cad5f2
Vilarj <lamaravillasan@outlook.com>
2021-01-12 10:03:53 -0400

    Old files deleted

--- home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from home import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('about/', views.about, name='about')
-]
--- home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse("Home")
-
-def about(request):
-    return HttpResponse("About me")
